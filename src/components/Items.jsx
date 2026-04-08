ecffdbf52c4f7a65e6a2ba3775d9d45667d284a3
csw030101 <csw030101@gmail.com>
2026-04-08 10:58:15 +0900

    현재 작업 중간 저장

---
+++ src/components/Items.jsx
@@ -0,0 +1,11 @@
+export default function Items ({name}) {
+    return (
+        <>
+        if({isPacked}) {
+            <li>{name} ✅</li>
+        }
+        <li>{name}</li>
+        </>
+    )
+}
+//123
